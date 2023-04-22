3b56266c68c43cc7332df8c1f1454d93fcbda066
Sophia Halasova <xhalas14@stud.fit.vutbr.cz>
2023-04-22 12:54:09 +0200

    Added comment

--- src/kalkulacka.py
+++ src/kalkulacka.py
@@ -1,10 +1,5 @@
+""" GUI for Numbo calculator"""
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'finishcalc.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.1
-#
-# WARNING! All changes made in this file will be lost!
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
